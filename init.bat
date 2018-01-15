789bd6583b21f3d33e0506d3c7fafda7d9e1daa7
stuman93 <stuartmurry93@gmail.com>
2018-01-15 12:46:50 -0700

    base-href fix

--- init.bat
+++ init.bat
@@ -1,5 +1,5 @@
 START /WAIT npm i -g angular-cli-ghpages
-START /WAIT ng build --prod --base-href "https://github.com/stuman93/didget.git"
+START /WAIT ng build --prod --base-href "https://stuman93.github.io/didget"
 START /WAIT git remote add origin https://github.com/stuman93/didget.git
 START /WAIT git push -u origin master
 START /WAIT ngh
